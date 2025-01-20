daac2cbf737658a58181be66a965fdba0bc06010
Robert-on-linux <rupp_robert@hotmail.com>
2025-01-20 16:33:29 +0100

    Parser correction

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,51 +1,38 @@
-return { -- Highlight, edit, and navigate code
+return {
+    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
         ensure_installed = {
-            --            'all',
             'lua',
-            --            'python',
             'javascript',
             'typescript',
             'vimdoc',
             'vim',
             'regex',
-            --            'terraform',
             'sql',
             'dockerfile',
-            --            'toml',
             'json',
-            --            'java',
-            --            'groovy',
-            --            'go',
-            'gitignore',
-            --            'graphql',
             'yaml',
             'make',
             'cmake',
             'c',
-            'cpp',
+            'cpp', -- C++ hinzugefügt
             'markdown',
             'markdown_inline',
             'bash',
-            --            'tsx',
             'css',
             'html',
         },
-        ignore_install = {},
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = {
             enable = true,
-            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-            --  If you are experiencing weird indenting issues, add the language to
-            --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-            --      additional_vim_regex_highlighting = { 'ruby' },
-            additional_vim_regex_highlighting = false,
+            additional_vim_regex_highlighting = false, -- Verhindert Konflikte mit der alten Regex-basierten Hervorhebung
+            disable = {},
         },
+
         incremental_selection = {
             enable = true,
             keymaps = {
@@ -54,19 +41,16 @@ return { -- Highlight, edit, and navigate code
                 scope_incremental = 'grc',
                 node_decremental = 'grm',
             },
-            indent = {
-                enable = true,
-                --            disable = { 'ruby' },
-            },
+        },
+        indent = {
+            enable = true,
         },
     },
 
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    -- Debugging-Modus aktivieren, um genauere Fehlerberichte zu erhalten
+    debug = true,
+
+    -- Textobjekte für nvim-treesitter (optional)
     textobjects = {
         select = {
             enable = true,
@@ -79,8 +63,14 @@ return { -- Highlight, edit, and navigate code
             },
         },
     },
+
+    -- Refactor-Optionen für nvim-treesitter (optional)
     refactor = {
         highlight_definitions = { enable = true },
         highlight_current_scope = { enable = false },
     },
+
+
+    -- Weitere nvim-treesitter-Module
+    -- Incremental selection, Textobjects, Refactor etc.
 }
